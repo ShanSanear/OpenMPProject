c83de0f5664d4b4dcd1b6d21ac95c721a6521ee1
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-03-02 18:40:44 +0100

    Adding b array.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 void for_loop_example() {
     int n = 1000;
-    int x;
     double a[1000];
+    double b[1000];
     bool firstLoop = false;
 
 #pragma omp parallel for private(firstLoop)
@@ -16,6 +16,7 @@ void for_loop_example() {
             printf("This is first run in this loop. Thread: %d Loop counter: %d\n", omp_get_thread_num(), j);
         }
         a[j] = 2 * j;
+        b[j] = b[j] * 2;
     }
 }
 
